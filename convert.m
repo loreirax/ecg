af4aaebb623581755b0c2bd6a58dbe0f363997e1
loreirax <35736747+loreirax@users.noreply.github.com>
2018-01-26 16:16:31 +0100

    Ecg di prova e script matlab per txt
    Aggiunta di un ecg di prova in formato .mat e .info e script convert.m per convertirlo in .txt (incluso file convertito)

---
+++ convert.m
@@ -0,0 +1,39 @@
+function convert(Name)
+infoName = strcat(Name, '.info');
+matName = strcat(Name, '.mat');
+Octave = exist('OCTAVE_VERSION');
+load(matName);
+fid = fopen(infoName, 'rt');
+fgetl(fid);
+fgetl(fid);
+fgetl(fid);
+[freqint] = sscanf(fgetl(fid), 'Sampling frequency: %f Hz  Sampling interval: %f sec');
+interval = freqint(2);
+fgetl(fid);
+
+if(Octave)
+    for i = 1:size(val, 1)
+       R = strsplit(fgetl(fid), char(9));
+       signal{i} = R{2};
+       gain(i) = str2num(R{3});
+       base(i) = str2num(R{4});
+       units{i} = R{5};
+    end
+else
+    for i = 1:size(val, 1)
+      [row(i), signal(i), gain(i), base(i), units(i)]=strread(fgetl(fid),'%d%s%f%f%s','delimiter','\t');
+    end
+end
+
+fclose(fid);
+val(val==-32768) = NaN;
+
+for i = 1:size(val, 1)
+    val(i, :) = (val(i, :) - base(i)) / gain(i);
+end
+load("ecg.txt");
+id = fopen("ecg.txt", "wt");
+fprintf(id, "%f \n", val');
+fclose(id);
+end
+

ce1dd46f914dfe3dd16a60558fceaabb8f58b0ca
loreirax <irace.lorenzo@gmail.com>
2018-01-31 12:16:15 +0100

    aggiunta frequenza cardiaca

--- qrs.c
+++ qrs.c
@@ -4,13 +4,21 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <math.h>
-#include <semaphore.h>
-#define nsample 3800
-#define ncampioni 30
+#define nsample 550 //campioni mostrati a video
+#define nsoglia 250 //numero campioni prima del calcolo della soglia
+#define ncampioni 4 //campioni per la condizione del picco
+#define white 15
+#define blue 1
+#define light_blue 9
+#define transparent -1
+#define black 0
+#define unit_x 1.72
+#define unit_y 150
 
 //condivisi
 float 	ecg[nsample];
 int 	index_in;
+int	contatore_qrs;
 
 //calcolo picco
 float	X[5];
@@ -18,32 +26,35 @@ float 	Y0[3];
 float 	Y1;
 float	Y2;
 float 	Y3;
-int 	contatore_soglia, primo_vincolo, contatore_qrs;
+int 	contatore_soglia, primo_vincolo;
 float 	maxValue, soglia1, soglia2;
 
-//colori
-int 	white, blue;
+//calcolo frequenza
+int	frequenza_attiva = 0;
+int	misure[5];
+int	index_m;
+BITMAP	*freq;
 
 //coordinate
 int 	_x1, _y1, _x2, _y2;
-int 	baseY =  200;//500;
-int 	baseX =  10;//100;
+int 	baseY =  200;
+int 	baseX =  10;
 BITMAP 	*grafico;
-int 	altezza = 400;//500;
-int 	lunghezza = 980; //600;
+int 	altezza = 400;
+int 	lunghezza = 980;
 
 //file dell'ecg
 FILE 	*ecg_file;
 
 //semafori
 pthread_mutex_t secg = PTHREAD_MUTEX_INITIALIZER;
-sem_t	event;
+pthread_mutex_t sqrs = PTHREAD_MUTEX_INITIALIZER;
 int	sync_calcolo, sync_grafico;
 pthread_mutex_t	sync_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t	sync_var;
 
 //thread
-pthread_t 	ecg_id, calc_id, graph_id;
+pthread_t 	ecg_id, calc_id, graph_id, freq_id;
 
 void setSoglie(float last){
 	if (last > maxValue){
@@ -57,8 +68,8 @@ void *task_ecg(){
 	int	h;
 	float 	dato = 0.0;
 	index_in = 0;
-	if ((ecg_file = fopen("ecg.txt", "r")) == NULL) {
-		printf("Non posso aprire il file %s\n", "ecg.txt");
+	if ((ecg_file = fopen("n01m.txt", "r")) == NULL) {
+		printf("Non posso aprire il file %s\n", "n01m.txt");
 		exit(1);
 	}
 	rewind(ecg_file);
@@ -68,7 +79,7 @@ void *task_ecg(){
 		ecg[index_in] = dato;
 		index_in = (index_in + 1) % nsample;
 		pthread_mutex_unlock(&secg);
-		usleep(10000);
+		usleep(7813);
 		//usleep(1388); //frequenza a 720 Hz
 	}
 	for(;;){
@@ -82,8 +93,36 @@ void *task_ecg(){
 		sync_grafico = 1;
 		pthread_mutex_unlock(&sync_mutex);
 		pthread_cond_broadcast(&sync_var);
-		usleep(20000);
+		usleep(7813);
 		//usleep(1388); //frequenza a 720 Hz
+	}
+}
+
+void *task_frequenza(){
+	int	begin = 0;
+	int	sum = 0;
+	int	l;
+	char	s[3];
+	index_m = 0;
+	sleep(2);
+	for(;;){
+		pthread_mutex_lock(&sqrs);
+		misure[index_m] = contatore_qrs;
+		contatore_qrs = 0;
+		pthread_mutex_unlock(&sqrs);
+		index_m = (index_m + 1) % 5;
+		if (begin < 4)
+			begin++;
+		else {
+			sum = 0;
+			for(l = 0; l < 5; ++l)
+				sum += misure[l];
+			sprintf(s,"%d", sum * 6);
+			clear_to_color(freq, blue);
+			textout_ex(freq, font, s, 0, 0, white, transparent);
+			blit(freq, screen, 0, 0, 100, 10, 20, 20);
+		}
+		sleep(2);
 	}
 }
 
@@ -92,7 +131,6 @@ void *task_calcolo(){
 	int	start = 0;
 	maxValue = 0;
 	contatore_soglia = 0;
-	contatore_qrs = 0;
 	for(;;) {
 		pthread_mutex_lock(&sync_mutex);
 		while(sync_calcolo == 0)
@@ -111,12 +149,16 @@ void *task_calcolo(){
 		Y2 = fabs(X[0] - 2 * X[2] + X[4]);
 		Y3 = Y1 + Y2;
 		setSoglie(Y3);
-		if (start <= 1000)
+		if (start <= nsoglia)
 			start++;
 		else {
-			if (Y3 >= soglia2 && primo_vincolo == 1){
+			if (frequenza_attiva == 0){
+				pthread_create(&freq_id, NULL, task_frequenza, NULL);
+				frequenza_attiva = 1;
+			}
+			if (Y3 >= soglia2 && primo_vincolo == 1)
 				contatore_soglia++;
-			} else {
+			else {
 				contatore_soglia = 0;
 				primo_vincolo = 0;
 			}
@@ -124,10 +166,12 @@ void *task_calcolo(){
 			if (Y3 >= soglia1 && primo_vincolo == 0)
 				primo_vincolo = 1;			
 			if (contatore_soglia == ncampioni) {
+				pthread_mutex_lock(&sqrs);
 				contatore_qrs++;
+				pthread_mutex_unlock(&sqrs);
 				contatore_soglia = 0;
 				primo_vincolo = 0;
-				printf("Picco numero %d\nMassimo %f\n", contatore_qrs, maxValue);
+				printf("Picco numero %d\n", contatore_qrs);
 			}
 		}	
 	}
@@ -142,13 +186,16 @@ void *task_grafico(){
 		sync_grafico = 0;
 		pthread_mutex_unlock(&sync_mutex);
 		clear_bitmap(grafico);
+		line(grafico, baseX, baseY, baseX + nsample * unit_x, baseY, light_blue); // 0mV
+		line(grafico, baseX, baseY - 1 * unit_y, baseX + nsample * unit_x, baseY - 1 * unit_y, light_blue); // 1mV
+		line(grafico, baseX, baseY + 1 * unit_y, baseX + nsample * unit_x, baseY + 1 * unit_y, light_blue); // -1mV
 		pthread_mutex_lock(&secg);
 		for(k = 1; k < nsample; ++k) {
 			if (k != index_in) {	
-				_x1 = baseX + (k - 1) * 0.25F;
-				_y1 = baseY - ecg[k - 1] * 150.0F;
-				_x2 = baseX + k * 0.25F;
-				_y2 = baseY - ecg[k] * 150.0F;
+				_x1 = baseX + (k - 1) * unit_x;
+				_y1 = baseY - ecg[k - 1] * unit_y;
+				_x2 = baseX + k * unit_x;
+				_y2 = baseY - ecg[k] * unit_y;
 				line(grafico, _x1, _y1, _x2, _y2, white);
 			}
 		}
@@ -161,14 +208,17 @@ int main(){
 	int 	m;
 	for (m = 0; m < nsample; ++m)
 		ecg[m] = 0.0;
+	contatore_qrs = 0;
 	pthread_cond_init(&sync_var, NULL);
 	allegro_init();
 	set_color_depth(8);
-	white = 15;
-	blue = 9;
-	grafico = create_bitmap(lunghezza, altezza);
 	set_gfx_mode(GFX_AUTODETECT_WINDOWED, 1080, 720, 0, 0);
 	clear_to_color(screen, blue);
+	grafico = create_bitmap(lunghezza, altezza);
+	freq = create_bitmap(20, 20);
+	clear_to_color(freq, blue);
+	textout_ex(freq, font, "--", 0, 0, white, transparent);
+	blit(freq, screen, 0, 0, 100, 10, 20, 20);
 	blit(grafico, screen, 0, 0, 50, 300, lunghezza, altezza);
 	pthread_create(&ecg_id, NULL, task_ecg, NULL);
 	pthread_create(&calc_id, NULL, task_calcolo, NULL);
